3c3e6fbc962797ae51d7c8a1cb30509fa7d74336
Antonio Guerreiro Borges <84242695+ANT-devolper@users.noreply.github.com>
2023-09-21 09:22:16 -0300

    Tipos Escalares

--- tipos-primitivos/index.php
+++ tipos-primitivos/index.php
@@ -8,8 +8,29 @@
 <body>
     <h1>Teste de tipos primitivos</h1>
     <?php 
-        $num = 0x1A;
-        echo "O valor da variavel é $num";
+        // 0x = hexadecimal
+        // 0b = binario
+        // 0 = Octal
+        // $num = 0x1A;
+        // echo "O valor da variavel é $num";
+        
+        // $v = false;
+        // var_dump($v);
+        
+        // $num = (float) "950";
+        // var_dump($num);
+
+        // $casado = false;
+        // echo "O valor para casado é $casado";
+
+        // $vetor = [6, 2, "ANT", 3, "gustavo"];
+        // var_dump($vetor);
+
+        class Pessoa {
+            private string $nome;
+        }
+        $p = new Pessoa;
+        var_dump($p);
     ?>
 </body>
 </html>
