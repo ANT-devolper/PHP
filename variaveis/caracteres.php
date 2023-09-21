d76749fd1639b1b53ef34063b5eab3b98a3110d5
Antonio Guerreiro Borges <84242695+ANT-devolper@users.noreply.github.com>
2023-09-20 22:17:12 -0300

    Tipos primitivos

---
+++ variaveis/caracteres.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Caracteres diferentes</title>
+</head>
+<body>
+    <h1>Variaveis com caracteres da tabela ASCII</h1>
+    <p>
+        <?php 
+          $valorÐ = "POG";
+          
+          echo "variavel = $valorÐ"
+        ?>
+    </p>
+</body>
+</html>
